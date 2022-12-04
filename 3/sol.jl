a8817325ff86764f22624a822066f18ae178eb16
Noé Blassel <noe.blassel@gmail.com>
2022-12-04 18:48:58 +0100

    format sol 3

--- 3/sol.jl
+++ 3/sol.jl
@@ -1,23 +1,16 @@
 @inline priority(c::Char) = Int(c) - 96 + (isuppercase(c) ? 58 : 0) 
 
 function sol(f)
-    sum_priorities=0
+    sum_priorities_a=0
+    sum_priorities_b=0
+
     lines= readlines(f)
-    for l in lines
-        N=Int(length(l)/2)
-        common_item = pop!(Set(l[1:N]) ∩ Set(l[N+1:end]))
-        sum_priorities += priority(common_item)
-    end
-    return sum_priorities
-end
-
-function sol2(f)
-    sum_priorities=0
-    lines=readlines(f)
     for (l1,l2,l3)=zip(lines[1:3:end],lines[2:3:end],lines[3:3:end])
-        common_item = pop!( Set(l1) ∩ Set(l2) ∩ Set(l3) )
-        sum_priorities += priority(common_item)
+        for l in (l1,l2,l3)
+            N=Int(length(l)/2)
+            sum_priorities_a+= priority(first(l[1:N] ∩ l[N+1:end]))
+        end
+        sum_priorities_b+= priority(first(l1 ∩ l2 ∩ l3))
     end
-
-    return sum_priorities
+    return sum_priorities_a,sum_priorities_b
 end
