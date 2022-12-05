77cb090de6860c6c2c520687d7c0fa24a77d3f61
Noé Blassel <noe.blassel@gmail.com>
2022-12-05 22:44:03 +0100

    simplify timing script

--- time_solutions.jl
+++ time_solutions.jl
@@ -5,18 +5,12 @@ for i=1:5
     include("$i/sol.jl")
 end
 
-modules=[:DayOne,:DayTwo,:DayThree,:DayFour,:DayFive]
+days=[DayOne,DayTwo,DayThree,DayFour,DayFive]
 
-for m=modules
-    Base.require(Main,m)
+for (i,day)=enumerate(days)
+    day.sol("$i/input")
 end
 
-module_list=eval.(Expr.(modules))
-
-for (i,m)=enumerate(module_list)
-    m.sol("$i/input")
-end
-
-@btime for (i,m)=enumerate(module_list)
-    m.sol("$i/input")
+@btime for (i,day)=enumerate(days)
+    day.sol("$i/input")
 end
