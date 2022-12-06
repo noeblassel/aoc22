ed78013ea848138f3e5ba055019809e5ea69de7e
Noé Blassel <noe.blassel@gmail.com>
2022-12-06 20:03:35 +0100

    fix day two and add solution test

--- 2/sol.jl
+++ 2/sol.jl
@@ -3,16 +3,16 @@ module DayTwo
 export sol
     function sol(f)
         lines=readlines(f)
-        scores=(3,6,0)
+        scores=(3,6,0) #draw, win, loss
         score_a= 0
         score_b=0
 
         for l in lines
-            op_move=Int(l[1])-65
-            me_move_a=Int(l[3])-88
-            me_move_b=mod(op_move+Int(l[3])-88,3)
-            score_a += (me_move_a+1) + scores[1+mod(me_move_a-op_move,3)]
-            score_b += (me_move_b+1) + scores[1+mod(me_move_b-op_move,3)]
+            op_move=Int(l[1])-65 # (A,B,C) -> (0,1,2) ≅ (rock,paper,scissors)
+            me_move_a=Int(l[3])-88 # (X,Y,Z) -> (0,1,2) ≅ (rock,paper,scissors)
+            me_move_b=mod(op_move+Int(l[3])-89,3) # (X,Y,Z) -> (-1,0,1) ≅ (loss,draw,win)
+            score_a += (me_move_a + 1) + scores[1+mod(me_move_a - op_move,3)]
+            score_b += (me_move_b + 1) + scores[1+mod(me_move_b - op_move,3)]
         end
 
         return score_a,score_b
--- time_solutions.jl
+++ time_solutions.jl
@@ -7,8 +7,15 @@ end
 
 days=[DayOne,DayTwo,DayThree,DayFour,DayFive,DaySix]
 
+sols=[(68467,203420),
+        (8890,10238),
+        (7763,2569),
+        (507,897),
+        ("RTGWZTHLD","STHGRZZFR"),
+        (1093,3534)]
+        
 for (i,day)=enumerate(days)
-    day.sol("$i/input")
+     @assert day.sol("$i/input")==sols[i]
 end
 
 @btime for (i,day)=enumerate(days)
