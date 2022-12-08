d6486d8928087a795ca3c40b8d5567344bd72b1c
Noé Blassel <noe.blassel@gmail.com>
2022-12-08 18:02:46 +0100

    fix setup.jl

--- setup.jl
+++ setup.jl
@@ -1,20 +1,22 @@
 #!/bin/env julia
-using HTTP
+using Downloads
+
 day_n=ARGS[1]
 token=read("session.txt",String)
-
+println(token)
 url= "https://adventofcode.com/2022/day/$day_n/input"
-mkdir(day_n)
+println(url)
+!isdir(day_n) && mkdir(day_n)
 
-sol_template="""Module Day$day_n
+sol_template="""module Day$day_n
     function sol(f)
         lines=readlines(f)
     end
 end
 """
 
+Downloads.download(url, joinpath(day_n,"input"), headers = Dict("Cookie"=>token,"User-Agent"=>"Julia Downloads library"))
+
 sol_file = open(joinpath(day_n,"sol.jl"),"w")
 print(sol_file,sol_template)
 close(sol_file)
-
-HTTP.request_uri(url,)
